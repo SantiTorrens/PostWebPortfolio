5885a356a125a7fcd5cc3389bba8488a014ac1fe
Santiago <santiago@dempathy.com>
2024-04-14 16:12:44 +0200

    Added ProtectedRoutes and Logout Functionality

--- app/src/components/Auth/Header.tsx
+++ app/src/components/Auth/Header.tsx
@@ -1,13 +1,21 @@
 import { ReactElement } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import userPlaceholder from '/user.png'
+import { useUserStore } from "../../store/userSlice";
 
 export default function Header(): ReactElement {
   const location = useLocation();
+  const { logout } = useUserStore();
+  const navigate = useNavigate();
 
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+
+  }
   return (
-    <div className="w-full bg-blue-400 px-4 py-2  flex justify-center ">
-      <div className="container flex flex-col md:flex-row mx-auto items-center">
+    <div className="flex justify-center w-full px-4 py-2 bg-blue-400 ">
+      <div className="container flex flex-col items-center mx-auto md:flex-row">
         <div className="flex flex-row">
           <Link
             className={`text-white bold  hover:text-black  px-6 py-2 rounded-xl ${location.pathname === "/" ? "text-black" : ""}`}
@@ -19,18 +27,24 @@ export default function Header(): ReactElement {
             className={`text-white bold  hover:text-black  px-6 py-2 rounded-xl ${location.pathname === "/posts" ? "text-black " : ""}`}
             to="/dashboard/posts"
           >
-            Posts Management
+            Posts
+          </Link>
+          <Link
+            className={`text-white bold  hover:text-black  px-6 py-2 rounded-xl ${location.pathname === "/posts" ? "text-black " : ""}`}
+            to="/dashboard/posts/favorites"
+          >
+            Favorite Posts
           </Link>
         </div>
-        <div className="gap-4 text-white flex mx-auto mt-4 md:mt-0 md:ml-auto md:mr-0">
+        <div className="flex gap-4 mx-auto mt-4 text-white md:mt-0 md:ml-auto md:mr-0">
 
-          <Link
+          <button
+            onClick={handleLogout}
             className={`text-white bold shadow-lg bg-blue-600 hover:text-black hover:bg-blue-300 px-6 py-2 rounded-xl ${location.pathname === "/login" ? "text-black bg-blue-300" : ""}`}
-            to="/"
           >
             Logout
-          </Link>
-          <img src={userPlaceholder} className="rounded-full  mr-4 w-10 h-10" />
+          </button>
+          <img src={userPlaceholder} className="w-10 h-10 mr-4 rounded-full" />
 
         </div>
       </div>
