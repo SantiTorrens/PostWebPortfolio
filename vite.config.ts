13c6879c164e6c30f52f58d9b771fe56004789cf
Santiago <santiago@dempathy.com>
2024-04-15 18:45:08 +0200

    build on root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "PostWebPortfolio"
+    base: "PostWebPortfolio",
     root: "./",
     server: {
         port: 3000,
