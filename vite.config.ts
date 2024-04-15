b25a2f7ac5ce4e0e2b7b4e03b6fcf2b30fae94f2
Santiago <santiago@dempathy.com>
2024-04-15 18:52:53 +0200

    build on root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ dns.setDefaultResultOrder("verbatim");
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "PostWebPortfolio",
     root: "./",
     server: {
         port: 3000,
@@ -15,9 +14,5 @@ export default defineConfig({
     },
     optimizeDeps: {
         include: ["@testing-library/react", "@testing-library/jest-dom"],
-    },
-    build: {
-        // Output directory for the production build
-        outDir: "dist", // This will set the output directory to 'dist' in the current directory
     },
 });
