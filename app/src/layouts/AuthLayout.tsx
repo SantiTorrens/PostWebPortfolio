ed4e17d3d43eb9a33d9d4d6156eceb961cd90afe
Santiago <santiago@dempathy.com>
2024-04-15 17:03:49 +0200

    added Breadcrumbs to auth layout

--- app/src/layouts/AuthLayout.tsx
+++ app/src/layouts/AuthLayout.tsx
@@ -1,17 +1,25 @@
 import { ReactElement, ReactNode } from 'react';
 import Header from '../components/Auth/Header';
 import Footer from '../components/Auth/Footer';
+import BreadCrumbs from '../components/BreadCrumbs';
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 
 export default function AuthLayout({ children }: AuthLayoutProps): ReactElement {
+  const crumbs = [
+    { id: 1, name: 'Dashboard', url: '/dashboard' },
+    { id: 2, name: 'Posts', url: '/dashboard/posts' },
+    { id: 3, name: 'Post Favorites', url: '/dashboard/posts/favorites' },
+  ];
+
   return (
 
-    <div className=" flex flex-col h-full">
+    <div className="flex flex-col h-full ">
       <Header></Header>
-      <div className="container mx-auto p-5  h-full ">
+      <div className="container flex flex-col h-full gap-4 p-5 mx-auto ">
+        <BreadCrumbs crumbs={crumbs} />
         {children}
       </div>
       <Footer></Footer>
